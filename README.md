90f09b9197438b2f3043971629d1631078c5325c
Leigh <leight@gmail.com>
2014-08-31 08:24:06 +0100

    Housekeeping

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 Poly1305 in PHP
 ===============
 
-This is a pure PHP implementation of the Poly1305 algorithm for those who can not, or do not want to install my [Poly1305 PHP extension](https://github.com/lt/php-poly1305)
+This library contains pure PHP implementations of the Poly1305 algorithm.
+
+ - The GMP based implementation requires the GMP extension and PHP 5.6 or above.
+ - The Native64 implementation requires PHP 5.4 or above, and 8 byte integers.
+ - The Native32 implementation requires PHP 5.4 or above
+
+The above implementations are listed in order of performance.
+
+For those who require even better performance, I have a C-based [Poly1305 PHP extension](https://github.com/lt/php-poly1305).
--- lib/Poly1305/Poly1305.php
+++ lib/Poly1305/Poly1305.php
@@ -2,7 +2,7 @@
 
 namespace Poly1305;
 
-if (extension_loaded('poly1305')) {
+if (0 && extension_loaded('poly1305')) {
     // TODO: Fix extension
 }
 else {
@@ -24,5 +24,20 @@ else {
     function poly1305_verify($authenticator, $key, $message) {
         $poly1305 = new Poly1305();
         return $poly1305->verify($authenticator, $key, $message);
+    }
+
+    if (!function_exists('hash_equals')) {
+        function hash_equals($known, $user)
+        {
+            $knownLen = strlen($known);
+            $userLen = strlen($user);
+            $result = $knownLen ^ $userLen;
+
+            for ($i = 0; $i < $knownLen; $i++) {
+                $result |= ord($known[$i] ^ $user[$i % $userLen]);
+            }
+
+            return $result === 0;
+        }
     }
 }
