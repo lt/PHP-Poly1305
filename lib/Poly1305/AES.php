7e4deec95425c2d257e3adf6281ed0dc2c5d4e64
Leigh <leight@gmail.com>
2014-09-07 16:53:50 +0100

    Rolled loops (negligible perf hit) and optimised final round

--- lib/Poly1305/AES.php
+++ lib/Poly1305/AES.php
@@ -5,9 +5,9 @@ namespace Poly1305;
 if (extension_loaded('openssl')) {
     class AES
     {
-        function kn($k, $n)
+        function kn($key, $nonce)
         {
-            return openssl_encrypt($n, 'aes-128-ecb', $k, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING);
+            return openssl_encrypt($nonce, 'aes-128-ecb', $key, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING);
         }
     }
 }
@@ -22,10 +22,10 @@ elseif (extension_loaded('mcrypt')) {
             $this->td = mcrypt_module_open('rijndael-128', null, 'ecb', null);
         }
 
-        function kn($k, $n)
+        function kn($key, $nonce)
         {
-            mcrypt_generic_init($this->td, $k, $k);
-            return mcrypt_generic($this->td, $n);
+            mcrypt_generic_init($this->td, $key, $key);
+            return mcrypt_generic($this->td, $nonce);
         }
     }
 }
@@ -33,6 +33,8 @@ elseif (extension_loaded('mcrypt')) {
 else {
     class AES
     {
+        private $rk;
+
         // Precomputed MixColumns tables
         private $T0 = [
             0xc66363a5, 0xf87c7c84, 0xee777799, 0xf67b7b8d, 0xfff2f20d, 0xd66b6bbd, 0xde6f6fb1, 0x91c5c554,
@@ -194,7 +196,7 @@ else {
             0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
         ];
 
-        function k($k)
+        function k($key)
         {
             $s = $this->S;
             list(,$rk0, $rk1, $rk2, $rk3) = unpack('N4', $key);
@@ -224,7 +226,7 @@ else {
             ];
         }
     
-        function n($n)
+        function n($nonce)
         {
             $t0 = $this->T0;
             $t1 = $this->T1;
@@ -233,61 +235,26 @@ else {
             $s = $this->S;
             $rk  = $this->rk;
     
-            list(,$x0, $x1, $x2, $x3) = unpack('N4', $n);
+            list(,$x0, $x1, $x2, $x3) = unpack('N4', $nonce);
     
             $x0 ^= $rk[0];
             $x1 ^= $rk[1];
             $x2 ^= $rk[2];
             $x3 ^= $rk[3];
-    
-            //r1
-            $y0 = $t0[$x0 >> 24 & 0xff] ^ $t1[$x1 >> 16 & 0xff] ^ $t2[$x2 >> 8 & 0xff] ^ $t3[$x3 & 0xff] ^ $rk[4];
-            $y1 = $t0[$x1 >> 24 & 0xff] ^ $t1[$x2 >> 16 & 0xff] ^ $t2[$x3 >> 8 & 0xff] ^ $t3[$x0 & 0xff] ^ $rk[5];
-            $y2 = $t0[$x2 >> 24 & 0xff] ^ $t1[$x3 >> 16 & 0xff] ^ $t2[$x0 >> 8 & 0xff] ^ $t3[$x1 & 0xff] ^ $rk[6];
-            $y3 = $t0[$x3 >> 24 & 0xff] ^ $t1[$x0 >> 16 & 0xff] ^ $t2[$x1 >> 8 & 0xff] ^ $t3[$x2 & 0xff] ^ $rk[7];
-    
-            // r2
-            $x0 = $t0[$y0 >> 24 & 0xff] ^ $t1[$y1 >> 16 & 0xff] ^ $t2[$y2 >> 8 & 0xff] ^ $t3[$y3 & 0xff] ^ $rk[8];
-            $x1 = $t0[$y1 >> 24 & 0xff] ^ $t1[$y2 >> 16 & 0xff] ^ $t2[$y3 >> 8 & 0xff] ^ $t3[$y0 & 0xff] ^ $rk[9];
-            $x2 = $t0[$y2 >> 24 & 0xff] ^ $t1[$y3 >> 16 & 0xff] ^ $t2[$y0 >> 8 & 0xff] ^ $t3[$y1 & 0xff] ^ $rk[10];
-            $x3 = $t0[$y3 >> 24 & 0xff] ^ $t1[$y0 >> 16 & 0xff] ^ $t2[$y1 >> 8 & 0xff] ^ $t3[$y2 & 0xff] ^ $rk[11];
-    
-            // r3
-            $y0 = $t0[$x0 >> 24 & 0xff] ^ $t1[$x1 >> 16 & 0xff] ^ $t2[$x2 >> 8 & 0xff] ^ $t3[$x3 & 0xff] ^ $rk[12];
-            $y1 = $t0[$x1 >> 24 & 0xff] ^ $t1[$x2 >> 16 & 0xff] ^ $t2[$x3 >> 8 & 0xff] ^ $t3[$x0 & 0xff] ^ $rk[13];
-            $y2 = $t0[$x2 >> 24 & 0xff] ^ $t1[$x3 >> 16 & 0xff] ^ $t2[$x0 >> 8 & 0xff] ^ $t3[$x1 & 0xff] ^ $rk[14];
-            $y3 = $t0[$x3 >> 24 & 0xff] ^ $t1[$x0 >> 16 & 0xff] ^ $t2[$x1 >> 8 & 0xff] ^ $t3[$x2 & 0xff] ^ $rk[15];
-    
-            // r4
-            $x0 = $t0[$y0 >> 24 & 0xff] ^ $t1[$y1 >> 16 & 0xff] ^ $t2[$y2 >> 8 & 0xff] ^ $t3[$y3 & 0xff] ^ $rk[16];
-            $x1 = $t0[$y1 >> 24 & 0xff] ^ $t1[$y2 >> 16 & 0xff] ^ $t2[$y3 >> 8 & 0xff] ^ $t3[$y0 & 0xff] ^ $rk[17];
-            $x2 = $t0[$y2 >> 24 & 0xff] ^ $t1[$y3 >> 16 & 0xff] ^ $t2[$y0 >> 8 & 0xff] ^ $t3[$y1 & 0xff] ^ $rk[18];
-            $x3 = $t0[$y3 >> 24 & 0xff] ^ $t1[$y0 >> 16 & 0xff] ^ $t2[$y1 >> 8 & 0xff] ^ $t3[$y2 & 0xff] ^ $rk[19];
-    
-            // r5
-            $y0 = $t0[$x0 >> 24 & 0xff] ^ $t1[$x1 >> 16 & 0xff] ^ $t2[$x2 >> 8 & 0xff] ^ $t3[$x3 & 0xff] ^ $rk[20];
-            $y1 = $t0[$x1 >> 24 & 0xff] ^ $t1[$x2 >> 16 & 0xff] ^ $t2[$x3 >> 8 & 0xff] ^ $t3[$x0 & 0xff] ^ $rk[21];
-            $y2 = $t0[$x2 >> 24 & 0xff] ^ $t1[$x3 >> 16 & 0xff] ^ $t2[$x0 >> 8 & 0xff] ^ $t3[$x1 & 0xff] ^ $rk[22];
-            $y3 = $t0[$x3 >> 24 & 0xff] ^ $t1[$x0 >> 16 & 0xff] ^ $t2[$x1 >> 8 & 0xff] ^ $t3[$x2 & 0xff] ^ $rk[23];
-    
-            // r6
-            $x0 = $t0[$y0 >> 24 & 0xff] ^ $t1[$y1 >> 16 & 0xff] ^ $t2[$y2 >> 8 & 0xff] ^ $t3[$y3 & 0xff] ^ $rk[24];
-            $x1 = $t0[$y1 >> 24 & 0xff] ^ $t1[$y2 >> 16 & 0xff] ^ $t2[$y3 >> 8 & 0xff] ^ $t3[$y0 & 0xff] ^ $rk[25];
-            $x2 = $t0[$y2 >> 24 & 0xff] ^ $t1[$y3 >> 16 & 0xff] ^ $t2[$y0 >> 8 & 0xff] ^ $t3[$y1 & 0xff] ^ $rk[26];
-            $x3 = $t0[$y3 >> 24 & 0xff] ^ $t1[$y0 >> 16 & 0xff] ^ $t2[$y1 >> 8 & 0xff] ^ $t3[$y2 & 0xff] ^ $rk[27];
-    
-            // r7
-            $y0 = $t0[$x0 >> 24 & 0xff] ^ $t1[$x1 >> 16 & 0xff] ^ $t2[$x2 >> 8 & 0xff] ^ $t3[$x3 & 0xff] ^ $rk[28];
-            $y1 = $t0[$x1 >> 24 & 0xff] ^ $t1[$x2 >> 16 & 0xff] ^ $t2[$x3 >> 8 & 0xff] ^ $t3[$x0 & 0xff] ^ $rk[29];
-            $y2 = $t0[$x2 >> 24 & 0xff] ^ $t1[$x3 >> 16 & 0xff] ^ $t2[$x0 >> 8 & 0xff] ^ $t3[$x1 & 0xff] ^ $rk[30];
-            $y3 = $t0[$x3 >> 24 & 0xff] ^ $t1[$x0 >> 16 & 0xff] ^ $t2[$x1 >> 8 & 0xff] ^ $t3[$x2 & 0xff] ^ $rk[31];
-    
-            // r8
-            $x0 = $t0[$y0 >> 24 & 0xff] ^ $t1[$y1 >> 16 & 0xff] ^ $t2[$y2 >> 8 & 0xff] ^ $t3[$y3 & 0xff] ^ $rk[32];
-            $x1 = $t0[$y1 >> 24 & 0xff] ^ $t1[$y2 >> 16 & 0xff] ^ $t2[$y3 >> 8 & 0xff] ^ $t3[$y0 & 0xff] ^ $rk[33];
-            $x2 = $t0[$y2 >> 24 & 0xff] ^ $t1[$y3 >> 16 & 0xff] ^ $t2[$y0 >> 8 & 0xff] ^ $t3[$y1 & 0xff] ^ $rk[34];
-            $x3 = $t0[$y3 >> 24 & 0xff] ^ $t1[$y0 >> 16 & 0xff] ^ $t2[$y1 >> 8 & 0xff] ^ $t3[$y2 & 0xff] ^ $rk[35];
-    
+
+            // r1 - r8
+            for ($i = 4; $i < 36;) {
+                $y0 = $t0[$x0 >> 24 & 0xff] ^ $t1[$x1 >> 16 & 0xff] ^ $t2[$x2 >> 8 & 0xff] ^ $t3[$x3 & 0xff] ^ $rk[$i++];
+                $y1 = $t0[$x1 >> 24 & 0xff] ^ $t1[$x2 >> 16 & 0xff] ^ $t2[$x3 >> 8 & 0xff] ^ $t3[$x0 & 0xff] ^ $rk[$i++];
+                $y2 = $t0[$x2 >> 24 & 0xff] ^ $t1[$x3 >> 16 & 0xff] ^ $t2[$x0 >> 8 & 0xff] ^ $t3[$x1 & 0xff] ^ $rk[$i++];
+                $y3 = $t0[$x3 >> 24 & 0xff] ^ $t1[$x0 >> 16 & 0xff] ^ $t2[$x1 >> 8 & 0xff] ^ $t3[$x2 & 0xff] ^ $rk[$i++];
+
+                $x0 = $t0[$y0 >> 24 & 0xff] ^ $t1[$y1 >> 16 & 0xff] ^ $t2[$y2 >> 8 & 0xff] ^ $t3[$y3 & 0xff] ^ $rk[$i++];
+                $x1 = $t0[$y1 >> 24 & 0xff] ^ $t1[$y2 >> 16 & 0xff] ^ $t2[$y3 >> 8 & 0xff] ^ $t3[$y0 & 0xff] ^ $rk[$i++];
+                $x2 = $t0[$y2 >> 24 & 0xff] ^ $t1[$y3 >> 16 & 0xff] ^ $t2[$y0 >> 8 & 0xff] ^ $t3[$y1 & 0xff] ^ $rk[$i++];
+                $x3 = $t0[$y3 >> 24 & 0xff] ^ $t1[$y0 >> 16 & 0xff] ^ $t2[$y1 >> 8 & 0xff] ^ $t3[$y2 & 0xff] ^ $rk[$i++];
+            }
+
             // r9
             $y0 = $t0[$x0 >> 24 & 0xff] ^ $t1[$x1 >> 16 & 0xff] ^ $t2[$x2 >> 8 & 0xff] ^ $t3[$x3 & 0xff] ^ $rk[36];
             $y1 = $t0[$x1 >> 24 & 0xff] ^ $t1[$x2 >> 16 & 0xff] ^ $t2[$x3 >> 8 & 0xff] ^ $t3[$x0 & 0xff] ^ $rk[37];
@@ -295,20 +262,15 @@ else {
             $y3 = $t0[$x3 >> 24 & 0xff] ^ $t1[$x0 >> 16 & 0xff] ^ $t2[$x1 >> 8 & 0xff] ^ $t3[$x2 & 0xff] ^ $rk[39];
     
             // r10
-            $x0 = $s[$y0 & 0xff] | ($s[$y0 >> 8 & 0xff] << 8) | ($s[$y0 >> 16 & 0xff] << 16) | $s[$y0 >> 24 & 0xff] << 24;
-            $x1 = $s[$y1 & 0xff] | ($s[$y1 >> 8 & 0xff] << 8) | ($s[$y1 >> 16 & 0xff] << 16) | $s[$y1 >> 24 & 0xff] << 24;
-            $x2 = $s[$y2 & 0xff] | ($s[$y2 >> 8 & 0xff] << 8) | ($s[$y2 >> 16 & 0xff] << 16) | $s[$y2 >> 24 & 0xff] << 24;
-            $x3 = $s[$y3 & 0xff] | ($s[$y3 >> 8 & 0xff] << 8) | ($s[$y3 >> 16 & 0xff] << 16) | $s[$y3 >> 24 & 0xff] << 24;
-    
             return pack('N4',
-                ($x0 & 0xff000000) ^ ($x1 & 0xff0000) ^ ($x2 & 0xff00) ^ ($x3 & 0xff) ^ $rk[40],
-                ($x1 & 0xff000000) ^ ($x2 & 0xff0000) ^ ($x3 & 0xff00) ^ ($x0 & 0xff) ^ $rk[41],
-                ($x2 & 0xff000000) ^ ($x3 & 0xff0000) ^ ($x0 & 0xff00) ^ ($x1 & 0xff) ^ $rk[42],
-                ($x3 & 0xff000000) ^ ($x0 & 0xff0000) ^ ($x1 & 0xff00) ^ ($x2 & 0xff) ^ $rk[43]
+                (($s[$y0 >> 24 & 0xff] << 24) ^ ($s[$y1 >> 16 & 0xff] << 16) ^ ($s[$y2 >> 8 & 0xff] << 8) ^ $s[$y3 & 0xff]) ^ $rk[40],
+                (($s[$y1 >> 24 & 0xff] << 24) ^ ($s[$y2 >> 16 & 0xff] << 16) ^ ($s[$y3 >> 8 & 0xff] << 8) ^ $s[$y0 & 0xff]) ^ $rk[41],
+                (($s[$y2 >> 24 & 0xff] << 24) ^ ($s[$y3 >> 16 & 0xff] << 16) ^ ($s[$y0 >> 8 & 0xff] << 8) ^ $s[$y1 & 0xff]) ^ $rk[42],
+                (($s[$y3 >> 24 & 0xff] << 24) ^ ($s[$y0 >> 16 & 0xff] << 16) ^ ($s[$y1 >> 8 & 0xff] << 8) ^ $s[$y2 & 0xff]) ^ $rk[43]
             );
         }
 
-        function kn($k, $n)
+        function kn($key, $nonce)
         {
             $t0 = $this->T0;
             $t1 = $this->T1;
@@ -316,85 +278,32 @@ else {
             $t3 = $this->T3;
             $s  = $this->S;
 
-            list(,$rk0, $rk1, $rk2, $rk3) = unpack('N4', $k);
-            list(, $x0,  $x1,  $x2,  $x3) = unpack('N4', $n);
+            list(,$rk0, $rk1, $rk2, $rk3) = unpack('N4', $key);
+            list(, $x0,  $x1,  $x2,  $x3) = unpack('N4', $nonce);
 
             $x0 ^= $rk0;
             $x1 ^= $rk1;
             $x2 ^= $rk2;
             $x3 ^= $rk3;
 
-            //r1
-            $rk4 = $rk0 ^ ($s[$rk3 >> 24 & 0xff] | ($s[$rk3 & 0xff] << 8) | ($s[$rk3 >> 8 & 0xff] << 16) | (($s[$rk3 >> 16 & 0xff] ^ 0x01) << 24));
-            $rk5 = $rk1 ^ $rk4; $rk6 = $rk2 ^ $rk5; $rk7 = $rk3 ^ $rk6;
+            // r1 - r8
+            for ($i = 4, $rc = 1; $i < 36; $i += 8, $rc = ($rc << 1) % 0xe5) {
+                $rk4 = $rk0 ^ ($s[$rk3 >> 24 & 0xff] | ($s[$rk3 & 0xff] << 8) | ($s[$rk3 >> 8 & 0xff] << 16) | (($s[$rk3 >> 16 & 0xff] ^ $rc) << 24));
+                $rk5 = $rk1 ^ $rk4; $rk6 = $rk2 ^ $rk5; $rk7 = $rk3 ^ $rk6;
 
-            $y0 = $t0[$x0 >> 24 & 0xff] ^ $t1[$x1 >> 16 & 0xff] ^ $t2[$x2 >> 8 & 0xff] ^ $t3[$x3 & 0xff] ^ $rk4;
-            $y1 = $t0[$x1 >> 24 & 0xff] ^ $t1[$x2 >> 16 & 0xff] ^ $t2[$x3 >> 8 & 0xff] ^ $t3[$x0 & 0xff] ^ $rk5;
-            $y2 = $t0[$x2 >> 24 & 0xff] ^ $t1[$x3 >> 16 & 0xff] ^ $t2[$x0 >> 8 & 0xff] ^ $t3[$x1 & 0xff] ^ $rk6;
-            $y3 = $t0[$x3 >> 24 & 0xff] ^ $t1[$x0 >> 16 & 0xff] ^ $t2[$x1 >> 8 & 0xff] ^ $t3[$x2 & 0xff] ^ $rk7;
+                $y0 = $t0[$x0 >> 24 & 0xff] ^ $t1[$x1 >> 16 & 0xff] ^ $t2[$x2 >> 8 & 0xff] ^ $t3[$x3 & 0xff] ^ $rk4;
+                $y1 = $t0[$x1 >> 24 & 0xff] ^ $t1[$x2 >> 16 & 0xff] ^ $t2[$x3 >> 8 & 0xff] ^ $t3[$x0 & 0xff] ^ $rk5;
+                $y2 = $t0[$x2 >> 24 & 0xff] ^ $t1[$x3 >> 16 & 0xff] ^ $t2[$x0 >> 8 & 0xff] ^ $t3[$x1 & 0xff] ^ $rk6;
+                $y3 = $t0[$x3 >> 24 & 0xff] ^ $t1[$x0 >> 16 & 0xff] ^ $t2[$x1 >> 8 & 0xff] ^ $t3[$x2 & 0xff] ^ $rk7;
 
-            // r2
-            $rk0 = $rk4 ^ ($s[$rk7 >> 24 & 0xff] | ($s[$rk7 & 0xff] << 8) | ($s[$rk7 >> 8 & 0xff] << 16) | (($s[$rk7 >> 16 & 0xff] ^ 0x02) << 24));
-            $rk1 = $rk5 ^ $rk0; $rk2 = $rk6 ^ $rk1; $rk3 = $rk7 ^ $rk2;
+                $rk0 = $rk4 ^ ($s[$rk7 >> 24 & 0xff] | ($s[$rk7 & 0xff] << 8) | ($s[$rk7 >> 8 & 0xff] << 16) | (($s[$rk7 >> 16 & 0xff] ^ $rc = ($rc << 1) % 0xe5) << 24));
+                $rk1 = $rk5 ^ $rk0; $rk2 = $rk6 ^ $rk1; $rk3 = $rk7 ^ $rk2;
 
-            $x0 = $t0[$y0 >> 24 & 0xff] ^ $t1[$y1 >> 16 & 0xff] ^ $t2[$y2 >> 8 & 0xff] ^ $t3[$y3 & 0xff] ^ $rk0;
-            $x1 = $t0[$y1 >> 24 & 0xff] ^ $t1[$y2 >> 16 & 0xff] ^ $t2[$y3 >> 8 & 0xff] ^ $t3[$y0 & 0xff] ^ $rk1;
-            $x2 = $t0[$y2 >> 24 & 0xff] ^ $t1[$y3 >> 16 & 0xff] ^ $t2[$y0 >> 8 & 0xff] ^ $t3[$y1 & 0xff] ^ $rk2;
-            $x3 = $t0[$y3 >> 24 & 0xff] ^ $t1[$y0 >> 16 & 0xff] ^ $t2[$y1 >> 8 & 0xff] ^ $t3[$y2 & 0xff] ^ $rk3;
-
-            // r3
-            $rk4 = $rk0 ^ ($s[$rk3 >> 24 & 0xff] | ($s[$rk3 & 0xff] << 8) | ($s[$rk3 >> 8 & 0xff] << 16) | (($s[$rk3 >> 16 & 0xff] ^ 0x04) << 24));
-            $rk5 = $rk1 ^ $rk4; $rk6 = $rk2 ^ $rk5; $rk7 = $rk3 ^ $rk6;
-
-            $y0 = $t0[$x0 >> 24 & 0xff] ^ $t1[$x1 >> 16 & 0xff] ^ $t2[$x2 >> 8 & 0xff] ^ $t3[$x3 & 0xff] ^ $rk4;
-            $y1 = $t0[$x1 >> 24 & 0xff] ^ $t1[$x2 >> 16 & 0xff] ^ $t2[$x3 >> 8 & 0xff] ^ $t3[$x0 & 0xff] ^ $rk5;
-            $y2 = $t0[$x2 >> 24 & 0xff] ^ $t1[$x3 >> 16 & 0xff] ^ $t2[$x0 >> 8 & 0xff] ^ $t3[$x1 & 0xff] ^ $rk6;
-            $y3 = $t0[$x3 >> 24 & 0xff] ^ $t1[$x0 >> 16 & 0xff] ^ $t2[$x1 >> 8 & 0xff] ^ $t3[$x2 & 0xff] ^ $rk7;
-
-            // r4
-            $rk0 = $rk4 ^ ($s[$rk7 >> 24 & 0xff] | ($s[$rk7 & 0xff] << 8) | ($s[$rk7 >> 8 & 0xff] << 16) | (($s[$rk7 >> 16 & 0xff] ^ 0x08) << 24));
-            $rk1 = $rk5 ^ $rk0; $rk2 = $rk6 ^ $rk1; $rk3 = $rk7 ^ $rk2;
-
-            $x0 = $t0[$y0 >> 24 & 0xff] ^ $t1[$y1 >> 16 & 0xff] ^ $t2[$y2 >> 8 & 0xff] ^ $t3[$y3 & 0xff] ^ $rk0;
-            $x1 = $t0[$y1 >> 24 & 0xff] ^ $t1[$y2 >> 16 & 0xff] ^ $t2[$y3 >> 8 & 0xff] ^ $t3[$y0 & 0xff] ^ $rk1;
-            $x2 = $t0[$y2 >> 24 & 0xff] ^ $t1[$y3 >> 16 & 0xff] ^ $t2[$y0 >> 8 & 0xff] ^ $t3[$y1 & 0xff] ^ $rk2;
-            $x3 = $t0[$y3 >> 24 & 0xff] ^ $t1[$y0 >> 16 & 0xff] ^ $t2[$y1 >> 8 & 0xff] ^ $t3[$y2 & 0xff] ^ $rk3;
-
-            // r5
-            $rk4 = $rk0 ^ ($s[$rk3 >> 24 & 0xff] | ($s[$rk3 & 0xff] << 8) | ($s[$rk3 >> 8 & 0xff] << 16) | (($s[$rk3 >> 16 & 0xff] ^ 0x10) << 24));
-            $rk5 = $rk1 ^ $rk4; $rk6 = $rk2 ^ $rk5; $rk7 = $rk3 ^ $rk6;
-
-            $y0 = $t0[$x0 >> 24 & 0xff] ^ $t1[$x1 >> 16 & 0xff] ^ $t2[$x2 >> 8 & 0xff] ^ $t3[$x3 & 0xff] ^ $rk4;
-            $y1 = $t0[$x1 >> 24 & 0xff] ^ $t1[$x2 >> 16 & 0xff] ^ $t2[$x3 >> 8 & 0xff] ^ $t3[$x0 & 0xff] ^ $rk5;
-            $y2 = $t0[$x2 >> 24 & 0xff] ^ $t1[$x3 >> 16 & 0xff] ^ $t2[$x0 >> 8 & 0xff] ^ $t3[$x1 & 0xff] ^ $rk6;
-            $y3 = $t0[$x3 >> 24 & 0xff] ^ $t1[$x0 >> 16 & 0xff] ^ $t2[$x1 >> 8 & 0xff] ^ $t3[$x2 & 0xff] ^ $rk7;
-
-            // r6
-            $rk0 = $rk4 ^ ($s[$rk7 >> 24 & 0xff] | ($s[$rk7 & 0xff] << 8) | ($s[$rk7 >> 8 & 0xff] << 16) | (($s[$rk7 >> 16 & 0xff] ^ 0x20) << 24));
-            $rk1 = $rk5 ^ $rk0; $rk2 = $rk6 ^ $rk1; $rk3 = $rk7 ^ $rk2;
-
-            $x0 = $t0[$y0 >> 24 & 0xff] ^ $t1[$y1 >> 16 & 0xff] ^ $t2[$y2 >> 8 & 0xff] ^ $t3[$y3 & 0xff] ^ $rk0;
-            $x1 = $t0[$y1 >> 24 & 0xff] ^ $t1[$y2 >> 16 & 0xff] ^ $t2[$y3 >> 8 & 0xff] ^ $t3[$y0 & 0xff] ^ $rk1;
-            $x2 = $t0[$y2 >> 24 & 0xff] ^ $t1[$y3 >> 16 & 0xff] ^ $t2[$y0 >> 8 & 0xff] ^ $t3[$y1 & 0xff] ^ $rk2;
-            $x3 = $t0[$y3 >> 24 & 0xff] ^ $t1[$y0 >> 16 & 0xff] ^ $t2[$y1 >> 8 & 0xff] ^ $t3[$y2 & 0xff] ^ $rk3;
-
-            // r7
-            $rk4 = $rk0 ^ ($s[$rk3 >> 24 & 0xff] | ($s[$rk3 & 0xff] << 8) | ($s[$rk3 >> 8 & 0xff] << 16) | (($s[$rk3 >> 16 & 0xff] ^ 0x40) << 24));
-            $rk5 = $rk1 ^ $rk4; $rk6 = $rk2 ^ $rk5; $rk7 = $rk3 ^ $rk6;
-
-            $y0 = $t0[$x0 >> 24 & 0xff] ^ $t1[$x1 >> 16 & 0xff] ^ $t2[$x2 >> 8 & 0xff] ^ $t3[$x3 & 0xff] ^ $rk4;
-            $y1 = $t0[$x1 >> 24 & 0xff] ^ $t1[$x2 >> 16 & 0xff] ^ $t2[$x3 >> 8 & 0xff] ^ $t3[$x0 & 0xff] ^ $rk5;
-            $y2 = $t0[$x2 >> 24 & 0xff] ^ $t1[$x3 >> 16 & 0xff] ^ $t2[$x0 >> 8 & 0xff] ^ $t3[$x1 & 0xff] ^ $rk6;
-            $y3 = $t0[$x3 >> 24 & 0xff] ^ $t1[$x0 >> 16 & 0xff] ^ $t2[$x1 >> 8 & 0xff] ^ $t3[$x2 & 0xff] ^ $rk7;
-
-            // r8
-            $rk0 = $rk4 ^ ($s[$rk7 >> 24 & 0xff] | ($s[$rk7 & 0xff] << 8) | ($s[$rk7 >> 8 & 0xff] << 16) | (($s[$rk7 >> 16 & 0xff] ^ 0x80) << 24));
-            $rk1 = $rk5 ^ $rk0; $rk2 = $rk6 ^ $rk1; $rk3 = $rk7 ^ $rk2;
-
-            $x0 = $t0[$y0 >> 24 & 0xff] ^ $t1[$y1 >> 16 & 0xff] ^ $t2[$y2 >> 8 & 0xff] ^ $t3[$y3 & 0xff] ^ $rk0;
-            $x1 = $t0[$y1 >> 24 & 0xff] ^ $t1[$y2 >> 16 & 0xff] ^ $t2[$y3 >> 8 & 0xff] ^ $t3[$y0 & 0xff] ^ $rk1;
-            $x2 = $t0[$y2 >> 24 & 0xff] ^ $t1[$y3 >> 16 & 0xff] ^ $t2[$y0 >> 8 & 0xff] ^ $t3[$y1 & 0xff] ^ $rk2;
-            $x3 = $t0[$y3 >> 24 & 0xff] ^ $t1[$y0 >> 16 & 0xff] ^ $t2[$y1 >> 8 & 0xff] ^ $t3[$y2 & 0xff] ^ $rk3;
+                $x0 = $t0[$y0 >> 24 & 0xff] ^ $t1[$y1 >> 16 & 0xff] ^ $t2[$y2 >> 8 & 0xff] ^ $t3[$y3 & 0xff] ^ $rk0;
+                $x1 = $t0[$y1 >> 24 & 0xff] ^ $t1[$y2 >> 16 & 0xff] ^ $t2[$y3 >> 8 & 0xff] ^ $t3[$y0 & 0xff] ^ $rk1;
+                $x2 = $t0[$y2 >> 24 & 0xff] ^ $t1[$y3 >> 16 & 0xff] ^ $t2[$y0 >> 8 & 0xff] ^ $t3[$y1 & 0xff] ^ $rk2;
+                $x3 = $t0[$y3 >> 24 & 0xff] ^ $t1[$y0 >> 16 & 0xff] ^ $t2[$y1 >> 8 & 0xff] ^ $t3[$y2 & 0xff] ^ $rk3;
+            }
 
             // r9
             $rk4 = $rk0 ^ ($s[$rk3 >> 24 & 0xff] | ($s[$rk3 & 0xff] << 8) | ($s[$rk3 >> 8 & 0xff] << 16) | (($s[$rk3 >> 16 & 0xff] ^ 0x1b) << 24));
@@ -409,16 +318,11 @@ else {
             $rk0 = $rk4 ^ ($s[$rk7 >> 24 & 0xff] | ($s[$rk7 & 0xff] << 8) | ($s[$rk7 >> 8 & 0xff] << 16) | (($s[$rk7 >> 16 & 0xff] ^ 0x36) << 24));
             $rk1 = $rk5 ^ $rk0; $rk2 = $rk6 ^ $rk1; $rk3 = $rk7 ^ $rk2;
 
-            $x0 = $s[$y0 & 0xff] | ($s[$y0 >> 8 & 0xff] << 8) | ($s[$y0 >> 16 & 0xff] << 16) | $s[$y0 >> 24 & 0xff] << 24;
-            $x1 = $s[$y1 & 0xff] | ($s[$y1 >> 8 & 0xff] << 8) | ($s[$y1 >> 16 & 0xff] << 16) | $s[$y1 >> 24 & 0xff] << 24;
-            $x2 = $s[$y2 & 0xff] | ($s[$y2 >> 8 & 0xff] << 8) | ($s[$y2 >> 16 & 0xff] << 16) | $s[$y2 >> 24 & 0xff] << 24;
-            $x3 = $s[$y3 & 0xff] | ($s[$y3 >> 8 & 0xff] << 8) | ($s[$y3 >> 16 & 0xff] << 16) | $s[$y3 >> 24 & 0xff] << 24;
-
             return pack('N4',
-                ($x0 & 0xff000000) ^ ($x1 & 0xff0000) ^ ($x2 & 0xff00) ^ ($x3 & 0xff) ^ $rk0,
-                ($x1 & 0xff000000) ^ ($x2 & 0xff0000) ^ ($x3 & 0xff00) ^ ($x0 & 0xff) ^ $rk1,
-                ($x2 & 0xff000000) ^ ($x3 & 0xff0000) ^ ($x0 & 0xff00) ^ ($x1 & 0xff) ^ $rk2,
-                ($x3 & 0xff000000) ^ ($x0 & 0xff0000) ^ ($x1 & 0xff00) ^ ($x2 & 0xff) ^ $rk3
+                (($s[$y0 >> 24 & 0xff] << 24) ^ ($s[$y1 >> 16 & 0xff] << 16) ^ ($s[$y2 >> 8 & 0xff] << 8) ^ $s[$y3 & 0xff]) ^ $rk0,
+                (($s[$y1 >> 24 & 0xff] << 24) ^ ($s[$y2 >> 16 & 0xff] << 16) ^ ($s[$y3 >> 8 & 0xff] << 8) ^ $s[$y0 & 0xff]) ^ $rk1,
+                (($s[$y2 >> 24 & 0xff] << 24) ^ ($s[$y3 >> 16 & 0xff] << 16) ^ ($s[$y0 >> 8 & 0xff] << 8) ^ $s[$y1 & 0xff]) ^ $rk2,
+                (($s[$y3 >> 24 & 0xff] << 24) ^ ($s[$y0 >> 16 & 0xff] << 16) ^ ($s[$y1 >> 8 & 0xff] << 8) ^ $s[$y2 & 0xff]) ^ $rk3
             );
         }
     }
